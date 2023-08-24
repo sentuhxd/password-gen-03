2f80a6c9bab841509f312760d97e690854d79088
sentuhxd <142236429+sentuhxd@users.noreply.github.com>
2023-08-24 15:17:38 -0400

    Added dom elements

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// Assignment Code
+// DOM ELEMENTS
 var generateBtn = document.querySelector("#generate");
+var bigChar = "";
+var smallChar = "";
+var numChar = "";
+var specialChar = "";
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Alerts Functions
 
-  passwordText.value = password;
-
+function howLong() {
+  passwordLong = prompt("How long would you like your password to be? choose wisely it can only be between 8-128 characters:");
 }
 
+
+// Write password to the #password input
+//function writePassword() {
+  //var password = generatePassword();
+ // var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
